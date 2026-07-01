288f50060e810b427ed2570e2f53c44621f659b9
Rizqy Fadhil Athallah <fadhil2005new@gmail.com>
2026-07-01 14:25:52 +0700

    fix dashboard static page

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { prisma } from "@/lib/prisma";
 import Link from "next/link";
+export const dynamic = "force-dynamic";
 
 export default async function DashboardAdmin() {
   const totalBuku = await prisma.buku.count();
